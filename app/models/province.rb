cd0dad134a5d8aa74b3cc768b6b3ccab16e4bcd1
katsia <kilunga@Academic.RRC.CA>
2025-11-26 00:01:41 -0600

    Add ActiveAdmin resources for Categories and Provinces

--- app/models/province.rb
+++ app/models/province.rb
@@ -2,4 +2,12 @@ class Province < ApplicationRecord
   has_many :customers
   validates :name, presence: true
   validates :gst, :pst, :hst, numericality: { greater_than_or_equal_to: 0 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["name", "gst", "pst", "hst", "created_at", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["customers"]
+  end
 end
